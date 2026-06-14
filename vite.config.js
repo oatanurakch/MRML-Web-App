e26a04cea0caec2e9cd86f7dce11a528f61b56ba
oatanurakch <oatty6106164@gmail.com>
2026-06-14 18:17:53 +0700

    chore(allow host): add allowedHosts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ export default defineConfig(({ mode }) => {
 
   return {
     server: {
+      // allowedHosts: env.VITE_ALLOW_HOSTS ? [env.VITE_ALLOW_HOSTS] : [],
       proxy: {
         '/api': {
           target: env.VITE_API_BASE_URL,// API base URL
           changeOrigin: true,
           secure: false,
-        }
+        },
       }
     }
   }
